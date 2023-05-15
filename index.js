6370444d0656d841ba8963d58d520bfa8cafae94
Nelson <mungainelle96@gmail.com>
2023-05-15 16:31:44 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const employee={
-    name:"Jasper",
-    streetAddress:"27 main street"
+    'name':'Nelson Mungai',
+    'street':'23 Jump Street'
 }
-function updateEmployeeWithKeyAndValue(obj,key,value){
+function updateEmployeeWithKeyAndValue(obj, key, value){
     const newObj={...obj}
-    newObj[key]=value;
-    return newObj;
+    newObj[key]=value
+    return newObj
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
-    obj[key]=value;
-    return obj;
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value
+    return obj
 }
-function deleteFromEmployeeByKey(obj,key){
+function deleteFromEmployeeByKey(obj, key){
     const newObj={...obj}
     delete newObj[key]
-    return newObj;
+    return newObj
 }
-function destructivelyDeleteFromEmployeeByKey(obj,key){
+function destructivelyDeleteFromEmployeeByKey(obj, key){
     delete obj[key]
-    return obj;
+    return obj
 }
